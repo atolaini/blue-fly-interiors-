d843c068c6c1d8c57d60cc4f91ea6f89822c8ff8
Adam Tolaini <adam.tolaini@gmail.com>
2018-10-13 14:45:36 +0100

    services page taking shape

--- src/assets/js/new.js
+++ src/assets/js/new.js
@@ -2,8 +2,8 @@ var path = document.querySelectorAll("svg path");
 var el = document.querySelector(".services-hero");
 var pathArr = [];
 var length;
-var height = document.querySelector(".services-hero").offsetHeight;
 
+console.log(path);
 function svgPaths() {
   for (var i = 0; i < path.length; i++) {
     pathArr.push(path[i]);
@@ -14,16 +14,17 @@ function svgPaths() {
     console.log(lengths);
     for (var x = 0; x < path.length; x++) {
       path[x].style.strokeDasharray = lengths;
-      path[x].style.strokeDashoffset = "-" + lengths;
+      path[x].style.strokeDashoffset = "-" + length;
     }
   });
 }
 
 window.addEventListener("scroll", function() {
+  var height = document.querySelector(".services-hero").offsetHeight;
   var scroll = this.scrollY;
-  if (scroll === height) {
-    pathArr.forEach(function(el) {
-      el.classList.add("animation");
+  if (scroll == height) {
+    pathArr.forEach(function(el1) {
+      el1.classList.add("animation");
     });
   }
 });
--- src/assets/js/owl.js
+++ src/assets/js/owl.js
@@ -1,22 +1,49 @@
 $(document).ready(function() {
+  //Home pager slider
   $(".slider").owlCarousel({
     loop: true,
+    center: true,
+    autoplay: true,
     margin: 10,
+    items: 2,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    touchDrag: true,
+    dots: true,
     responsiveClass: true,
-
     responsive: {
       0: {
-        items: 1,
-        nav: true
+        items: 1
       },
       600: {
-        items: 3,
-        nav: false
+        items: 2
       },
       1000: {
-        items: 5,
-        nav: true,
-        loop: false
+        items: 2
+      }
+    }
+  });
+
+  //Home pager slider
+  $(".services-content__slider").owlCarousel({
+    loop: true,
+    center: true,
+    autoplay: true,
+    margin: 10,
+    items: 3,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    touchDrag: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 2
       }
     }
   });
